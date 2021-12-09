ef27cacde06df64b5bcdf867bae4de7c5e37578e
Athanasios Kastoras <akastoras@uth.gr>
2021-12-09 23:57:13 +0200

    Refactor comments

--- linked_lists.h
+++ linked_lists.h
@@ -1,7 +1,7 @@
 /*
  *  The following Functions are used to implement a simple
  * linked list sorted by strings with pointers to
- * undefined-datatype data.
+ * non preundefined data.
  * 
  * Code by: Athanasios Kastoras
  * Electrical and Computer Engineering | University of Thessaly
@@ -24,60 +24,71 @@ typedef struct linked_list {
 
 /*   Functions   */
 
-linked_list *init_linked_list();
 /*
  * Initialize new linked list, returns pointer to it. NULL for fail.
- */   
+ */ 
+linked_list *init_linked_list();
+
+/*
+ * create a node, set data pointer to struct data. NULL for fail.
+ */
 node *init_node(char *string, void *data);
-//create a node, set data pointer to struct data. NULL for fail.
 
+/*
+ * Adds new_node after old_node.
+ */
 void insert_after_node(node *old_node, node *new_node);
-//Adds new_node after old_node.
 
-int insert_node_to_linked_list(linked_list *ll, node *new_node);
 /* 
  * Adds a new node to a linked list.
  * return value: 0 -> success
  *               1 -> node is NULL
  */
+int insert_node_to_linked_list(linked_list *ll, node *new_node);
 
-int insert_to_linked_list(linked_list *ll, char *string, void *data);
 /*
  * Create node and insert it to list
  * return value: 0 -> success
  *               1 -> existing string
  */
+int insert_to_linked_list(linked_list *ll, char *string, void *data);
 
-node *find_in_linked_list_previous(linked_list *ll, char *string);
 /*
  * Finds the node of the given string and returns pointer to the previous node.
  * Else returns NULL.
  */
-node *find_in_linked_list(linked_list *ll, char *string);
+node *find_in_linked_list_previous(linked_list *ll, char *string);
+
 /*
  * Finds the node of the given string and returns pointer to it.
  * Else returns NULL.
  */
-int delete_next_node(node *prev_node);
+node *find_in_linked_list(linked_list *ll, char *string);
+
 /*
  *Deletes the node after the given node
  *Return value: 0 -> success
  *              1 -> not existing node
  */
-int delete_from_linked_list(linked_list *ll, char *string);
+int delete_next_node(node *prev_node);
+
 /* 
  * Deletes a new node after old node.
  * Return value: 0 -> success
  *               1 -> node is NULL
  */
+int delete_from_linked_list(linked_list *ll, char *string);
 
-void delete_linked_list(linked_list *ll);
+
 /*
  * Deletes linked list.
  */
-void print_linked_list(linked_list *ll);
+void delete_linked_list(linked_list *ll);
+
 /*
  * Prints all nodes (just the strings).
  */
+void print_linked_list(linked_list *ll);
+
 
 #endif  //!__LINKED_LISTS__H__
